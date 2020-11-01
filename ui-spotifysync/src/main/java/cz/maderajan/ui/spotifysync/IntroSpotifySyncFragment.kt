5b11b3814f5d90bcf35fbde3dc4e367e5e83d723
Jan Maděra <gmaderajan@gmail.com>
2020-11-01 22:55:27 +0100

    refactor

--- ui-spotifysync/src/main/java/cz/maderajan/ui/spotifysync/IntroSpotifySyncFragment.kt
+++ ui-spotifysync/src/main/java/cz/maderajan/ui/spotifysync/IntroSpotifySyncFragment.kt
@@ -1,9 +1,5 @@
 package cz.maderajan.ui.spotifysync
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import cz.maderajan.common.ui.fragment.viewBinding
 import cz.maderajan.ui.spotifysync.databinding.FragmentIntroSpotifySyncBinding
