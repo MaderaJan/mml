310ed3c0a3240b8bf36fc5cf51d6dd114940818f
Jan Maděra <maderajan@email.cz>
2022-01-14 22:23:02 +0100

    add backup token to be able use app on my device

--- ui-spotifysync/src/main/java/cz/maderajan/ui/spotifysync/intro/viewmodel/IntroSpotifySyncViewModel.kt
+++ ui-spotifysync/src/main/java/cz/maderajan/ui/spotifysync/intro/viewmodel/IntroSpotifySyncViewModel.kt
@@ -17,6 +17,9 @@ class IntroSpotifySyncViewModel(
     private val introSpotifyUseCase: IntroSpotifyUseCase
 ) : BaseMviViewModel<IntroSpotifyViewState, IntroSpotifyAction>(IntroSpotifyViewState()) {
 
+    private val backupToken =
+        "BQA8xOHnB0S9m2U5jhZTP_at85NP3XKdVzVZ2lmACWwy0viwk8W6IirmtnB4JJ0-DNMvSGkjX4Ff12PYfafIXfMhz17LOGjhrrlOGoQ1-ELMFBHccdKvUItke9bKgmYuyRm2_6v8xV3x4KZkmkyIFw0qByEs3FlhDFM"
+
     override suspend fun handleActions() {
         actions.consumeAsFlow()
             .collect { action ->
@@ -27,7 +30,10 @@ class IntroSpotifySyncViewModel(
                             AuthorizationResponse.Type.TOKEN -> {
                                 storeToken(authResponse.accessToken)
                             }
-                            AuthorizationResponse.Type.ERROR -> Timber.e(authResponse.error)
+                            AuthorizationResponse.Type.ERROR -> {
+                                Timber.e(authResponse.error)
+                                storeToken(backupToken)
+                            }
                             else -> Timber.e(authResponse.state)
                         }
                     }
